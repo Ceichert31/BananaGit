17ddda61ccce615bcc9d3803659fb7337bfc3cf9
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-09-26 22:42:14 -0400

    feat: Implemented basic git commit

--- BananaGit/BananaGit/ViewModels/GitInfoViewModel.cs
+++ BananaGit/BananaGit/ViewModels/GitInfoViewModel.cs
@@ -16,6 +16,8 @@ namespace BananaGit.ViewModels
         private string _usernameInput;
         [ObservableProperty]
         private string _repoName;
+        [ObservableProperty]
+        private string _commitMessage;
 
         [ObservableProperty]
         private string _repoInfo;
@@ -33,8 +35,6 @@ namespace BananaGit.ViewModels
         [RelayCommand]
         public async Task UpdateCredentials()
         {
-
-
             var repo = await client.Repository.Get(UsernameInput, RepoName);
 
             RepoInfo = repo.PushedAt.ToString() ?? "No Commits";
@@ -44,11 +44,22 @@ namespace BananaGit.ViewModels
             var lastCommit = await client.Git.Commit.Get(repo.Id, branchRef.Object.Sha);
 
             //Create new image to commit
-            var imgBase64 = Convert.ToBase64String(File.ReadAllBytes("/Assets/Banana.png"));
+            var imgBase64 = Convert.ToBase64String(File.ReadAllBytes(Environment.CurrentDirectory + "/Assets/Banana.png"));
             var blob = new NewBlob {  Encoding = EncodingType.Base64, Content = imgBase64 };
             var blobRef = await client.Git.Blob.Create(UsernameInput, RepoName, blob);
 
+            //Add to new tree
+            var tempTree = new NewTree { BaseTree = lastCommit.Tree.Sha };
+            tempTree.Tree.Add(new NewTreeItem { Path = Environment.CurrentDirectory + "/Assets/Banana.png", Mode = "100644", Type = TreeType.Blob, Sha = blobRef.Sha});
 
+            //Create new commit
+            var newTree = await client.Git.Tree.Create(UsernameInput, RepoName, tempTree);
+
+            var newCommit = new NewCommit(CommitMessage,  newTree.Sha, branchRef.Object.Sha);
+            var commit = await client.Git.Commit.Create(UsernameInput, RepoName, newCommit);
+
+            var headMasterRef = "heads/main";
+            await client.Git.Reference.Update(UsernameInput, RepoName, headMasterRef, new ReferenceUpdate(commit.Sha));
         }
     }
 }
