59ac7e94acab8f4f976a8b542c3eab5cca767647
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-09-26 22:25:22 -0400

    feat: Creating commit logic

---
+++ BananaGit/BananaGit/Utilities/XmlManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Octokit;
+
+namespace BananaGit.Utilities
+{
+    static public class XmlManager
+    {
+        /// <summary>
+        /// Saves the users personal github credential
+        /// </summary>
+        /// <param name="credentials">The personal credential</param>
+        public static void SaveGithubCredentials(string credentials)
+        {
+            //XmlTextReader textReader = new XmlTextReader(Environment.CurrentDirectory + "credential.txt");
+            XmlTextWriter textWriter = new(Environment.CurrentDirectory + "credential.txt", null); 
+
+            textWriter.WriteStartDocument();
+            textWriter.WriteComment(credentials);
+        }
+
+       /* public static string GetCredentials()
+        {
+            XmlDocument document = new XmlDocument();
+            XmlTextReader textReader = new XmlTextReader(Environment.CurrentDirectory + "credential.txt");
+
+            textReader.Read();
+         
+            document.Load(textReader);
+
+            Stream stream;
+            document.Save();
+
+            return null;
+        }*/
+    }
+}
--- BananaGit/BananaGit/ViewModels/GitInfoViewModel.cs
+++ BananaGit/BananaGit/ViewModels/GitInfoViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,16 +22,33 @@ namespace BananaGit.ViewModels
 
         private GitHubClient client;
 
+        private const string GITHUB_TOKEN = "";
+
         public GitInfoViewModel() 
         {
             client = new GitHubClient(new ProductHeaderValue("BananaGit"));
+            client.Credentials = new Credentials(GITHUB_TOKEN);
         }
 
         [RelayCommand]
         public async Task UpdateCredentials()
         {
+
+
             var repo = await client.Repository.Get(UsernameInput, RepoName);
+
             RepoInfo = repo.PushedAt.ToString() ?? "No Commits";
+
+            //Get latest commit from main
+            var branchRef = await client.Git.Reference.Get(repo.Id, "heads/main");
+            var lastCommit = await client.Git.Commit.Get(repo.Id, branchRef.Object.Sha);
+
+            //Create new image to commit
+            var imgBase64 = Convert.ToBase64String(File.ReadAllBytes("/Assets/Banana.png"));
+            var blob = new NewBlob {  Encoding = EncodingType.Base64, Content = imgBase64 };
+            var blobRef = await client.Git.Blob.Create(UsernameInput, RepoName, blob);
+
+
         }
     }
 }
