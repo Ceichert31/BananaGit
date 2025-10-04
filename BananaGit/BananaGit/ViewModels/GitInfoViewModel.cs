fd824e9ebca66f50f665ddd332c2ef6e5c18a48e
Ceichert31 <ceichert3114@gmail.com>
2025-10-04 19:38:15 -0400

    refactor: Changed caught exceptions from lib2git exceptions to all exceptions

--- BananaGit/BananaGit/ViewModels/GitInfoViewModel.cs
+++ BananaGit/BananaGit/ViewModels/GitInfoViewModel.cs
@@ -287,7 +287,7 @@ namespace BananaGit.ViewModels
                     HasCommitedFiles = true;
                 }
             }
-            catch (LibGit2SharpException)
+            catch (Exception)
             {
                 OutputError($"Failed to commit {LocalRepoFilePath}");
             }
