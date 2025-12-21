379faf6beeb665fdf7b2eb33711459b34ceb4b90
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-12-21 11:53:37 -0500

    chore: Created vm for commitHistory view

---
+++ BananaGit/BananaGit/ViewModels/CommitHistoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BananaGit.ViewModels;
+
+public class CommitHistoryViewModel : ObservableObject
+{
+    //Only needs to load repo info to display git commit history
+    //Doesn't need git service
+}
