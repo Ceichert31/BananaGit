84c1cfcf426b6e2f6441765692c121b1b7220761
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-12-21 12:38:04 -0500

    refactor: Added event to loadedRepositoryInfo to update vm info

--- BananaGit/BananaGit/Models/GitInfoModel.cs
+++ BananaGit/BananaGit/Models/GitInfoModel.cs
@@ -46,10 +46,66 @@
     /// <param name="url">The url for the saved repository</param>
     public class LoadedRepositoryInfo(string path, string url)
     {
-        public bool IsRepositoryCloned { get; set; } = false;
-        public GitBranch CurrentBranch { get; set; } = new GitBranch();
-        public string FilePath { get; set; } = path;
-        public string Url { get; set; } = url;
+        /// <summary>
+        /// Whether a repository is cloned or not
+        /// </summary>
+        public bool IsRepositoryCloned
+        {
+            get => _isRepositoryCloned;
+            set
+            {
+                _isRepositoryCloned = value;
+                OnRepositoryChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+        private bool _isRepositoryCloned = false;
+
+        /// <summary>
+        /// The current branch that changes will be made to
+        /// </summary>
+        public GitBranch CurrentBranch
+        {
+            get => _currentBranch;
+            set
+            {
+                _currentBranch = value;
+                OnRepositoryChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+        private GitBranch _currentBranch = new GitBranch();
+
+        /// <summary>
+        /// The file path to the local repository
+        /// </summary>
+        public string FilePath
+        {
+            get => _path;
+            set
+            {
+                _path = value;
+                OnRepositoryChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+        private string _path = path;
+
+        /// <summary>
+        /// The URL to the remote repository
+        /// </summary>
+        public string Url
+        {
+            get => _url;
+            set
+            {
+                _url = value;
+                OnRepositoryChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+        private string _url = url;
+
+        /// <summary>
+        /// Invoked when LoadedRepository properties are changed
+        /// </summary>
+        public EventHandler OnRepositoryChanged { get; }
     }
     /// <summary>
     /// Move this to its own model
