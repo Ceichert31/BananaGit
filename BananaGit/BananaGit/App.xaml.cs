a028cf8279714422dfcbd24a7f50df25eb0f2ebd
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2026-08-29 19:15:48 -0400

    chore: Added Velopack dependency

--- BananaGit/BananaGit/App.xaml.cs
+++ BananaGit/BananaGit/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Velopack;
 
 namespace BananaGit
 {
@@ -9,6 +10,27 @@ namespace BananaGit
     /// </summary>
     public partial class App : Application
     {
-    }
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            try
+            {
+                VelopackApp.Build()
+                    .OnFirstRun((v) =>
+                    {
+                        MessageBox.Show(v.Version.ToString(), "Version", MessageBoxButton.OK,
+                            MessageBoxImage.Information);
+                    })
+                    .Run();
 
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unhandled exception: " + ex.ToString());
+            }
+        }
+    }
 }
